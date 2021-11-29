e33ce12d05a677f03b6667d6f5b630f1971745be
alexs <alexplus77@mail.ru>
2021-11-29 14:21:28 +0200

    commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import "App.css";
 const Form = ({ handleSubmit, handleChange, value }) => (
   <form onSubmit={handleSubmit} className="form">
     <textarea value={value} onChange={handleChange} required={true} />
-    <button className="btn-send" onSubmit={handleSubmit}>
+    <button className="btn-send" onSubmit={handleSubmit} disabled={!value}>
       {" "}
       <i className="fa fa-space-shuttle" aria-hidden="true" />
     </button>
